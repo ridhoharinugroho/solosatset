c5aa254bc102c582c608a79e8b57d5812d03c56e
ridhoharinugroho <ridho.harinugroho@gmail.com>
2026-09-06 17:13:54 +0700

    security: tighten track-interest cors

--- api/track-interest.js
+++ api/track-interest.js
@@ -10,8 +10,6 @@ function getAdminClient() {
 }
 
 export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'POST,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Content-Type');
   if (req.method === 'OPTIONS') return res.status(204).end();
